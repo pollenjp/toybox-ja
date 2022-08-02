a676cf7d1300e2f283734d43e449da7a2242c1ab
pollenjp <polleninjp@gmail.com>
2022-08-02 21:42:40 +0900

    skeleton/section02/step02/

--- skeleton/section02/step02/main.go
+++ skeleton/section02/step02/main.go
@@ -1,28 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+const tmplStr = `
+Q. 以下のコードコンパイルして実行するとどうなるか？
+package main
+func main() {
+	true := false
+	println(true == false)
+}
+
+1: コンパイルエラー
+2: trueと表示される
+3: falseと表示される
+4: パニックが起きる
+`
 
 func main() {
 
-	fmt.Println("Q. 以下のコードコンパイルして実行するとどうなるか？")
-	fmt.Println("package main")
-	fmt.Println("func main() {")
-	fmt.Println("	true := false")
-	fmt.Println("	println(true == false)")
-	fmt.Println("}")
-
-	fmt.Println("1: コンパイルエラー")
-	fmt.Println("2: trueと表示される")
-	fmt.Println("3: falseと表示される")
-	fmt.Println("4: パニックが起きる")
+	fmt.Fprint(os.Stdout, tmplStr)
 
 	var answer int
+
 	fmt.Print("回答>")
 	fmt.Scanln(&answer)
-	
-	if /* TODO: answerが2の場合 */ {
+
+	if answer == 2 {
 		fmt.Println("正解!")
-	} /* TODO: それ以外の場合 */ {
+	} else {
 		fmt.Println("不正解!")
 		fmt.Println("答えは2です")
 	}
